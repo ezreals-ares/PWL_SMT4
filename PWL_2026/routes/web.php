94f06592a13c664ae41659d3bffb922790c261c5
ezreals-ares <nawafazril303@gmail.com>
2026-02-21 10:09:06 +0700

    push percobaan 6, 7, 8 & 9

--- PWL_2026/routes/web.php
+++ PWL_2026/routes/web.php
@@ -7,7 +7,16 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PhotoController;
-Route::resource('photos', PhotoController::class);
+
+Route::get('greeting', [WelcomeController::class, 'greeting']);
+
+Route::resource('photos', PhotoController::class)-> only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)-> except([
+    'create', 'store', 'update', 'destroy'
+]);
 
 Route::get('/hello', [WelcomeController::class,'hello']);
 
