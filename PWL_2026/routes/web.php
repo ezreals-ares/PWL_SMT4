36ec794ecaa880ebe5c1978e4de16efe73504e53
ezreals-ares <nawafazril303@gmail.com>
2026-02-21 09:07:19 +0700

    push laravel & Percobaan 4 & 5

--- PWL_2026/routes/web.php
+++ PWL_2026/routes/web.php
@@ -1,22 +1,25 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+Route::resource('photos', PhotoController::class);
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+Route::get('/hello', [WelcomeController::class,'hello']);
 
 Route::get('/world', function() {
     return 'World';
 });
 
-Route::get('/', function() {
-    return 'Selamat Datang';
-});
+Route::get('/', [HomeController::class,'index']);
+Route::get('/about', [AboutController::class,'about']);
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
 
-Route::get('/about', function() {
-    return 'Nama: Nawaf Azril Annaufal, NIM: 244107020047';
-});
+
 
 // Route::get('/user/{name}', function ($name) {
 //     return 'Nama saya '.$name;
@@ -26,16 +29,13 @@ Route::get('/posts/{posts}/comments/{comment}', function ($postID, $commentID) {
     return 'Pos ke-'.$postID." Komentar ke-: ".$commentID;
 });
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID '.$id;
-});
 
 // Route::get('/user/{name?}', function ($name='John') {
 //     return 'Nama Saya '.$name;
 // });
 
 // Route Name
-Route::get('/user/profile', function () {})->name('profile');
+// Route::get('/user/profile', function () {})->name('profile');
 
 // Route::get(
 // '/user/profile',
@@ -43,25 +43,25 @@ Route::get('/user/profile', function () {})->name('profile');
 // )->name('profile');
 
 // Generating URLs...
-$url = route('profile');
+// $url = route('profile');
 
 // Generating Redirects...
-return redirect()->route('profile');
+// return redirect()->route('profile');
 
 // Route Group dan Route Prefixes
-Route::middleware(['first', 'second'])->group(function () {
-Route::get('/', function () {
-// Uses first & second middleware...
-});
-Route::get('/user/profile', function () {
-// Uses first & second middleware...
-});
-});
-Route::domain('{account}.example.com')->group(function () {
-Route::get('user/{id}', function ($account, $id) {
-//
-});
-});
+// Route::middleware(['first', 'second'])->group(function () {
+// Route::get('/', function () {
+// // Uses first & second middleware...
+// });
+// Route::get('/user/profile', function () {
+// // Uses first & second middleware...
+// });
+// });
+// Route::domain('{account}.example.com')->group(function () {
+// Route::get('user/{id}', function ($account, $id) {
+// //
+// });
+// });
 // Route::middleware('auth')->group(function () {
 // Route::get('/user', [UserController::class, 'index']);
 // Route::get('/post', [PostController::class, 'index']);
@@ -76,8 +76,8 @@ Route::get('user/{id}', function ($account, $id) {
 // });
 
 // Redirect Routes
-Route::redirect('/here', '/there');
+// Route::redirect('/here', '/there');
 
 // View Rooutes
-Route::view('/welcome', 'welcome');
-Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+// Route::view('/welcome', 'welcome');
+// Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
