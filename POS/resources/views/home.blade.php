a28dabc37b29c1fdf0296d7b7e2279f7869207bc
ezreals-ares <nawafazril303@gmail.com>
2026-02-21 11:14:42 +0700

    Push Project POS (Tugas Praktikum)

---
+++ POS/resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>Home Page</h1>
+        <p>Selamat datang di halaman utama</p>
+    </body>
+</html>
---
+++ POS/resources/views/sales/pos.blade.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+    <h1>Halaman Penjualan</h1>
+    <p>Transaksi penjualan dilakukan di halaman ini</p>
+</body>
+</html>
