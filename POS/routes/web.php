2ba597d19e81c84e6f0a928c1d68bd2ac121fb32
ezreals-ares <nawafazril303@gmail.com>
2026-02-24 04:33:29 +0700

    push praktikum 4

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/level', [LevelController::class, 'index']);
 
 Route::get('/', [HomeController::class, 'index']);
 
