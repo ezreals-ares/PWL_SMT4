4f6d6229a28a664a72e36eca69dcec5ae56a295d
ezreals-ares <nawafazril303@gmail.com>
2026-02-24 07:05:44 +0700

    push praktikum 6

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function profile($id, $name) {
-        return view('user.profile', [
-            
-            'id' => $id,
-            'name' => $name
-        ]);
-    }
+  public function index() {
+    $data = [
+        'nama' => 'Pelanggan Pertama',
+    ];
+    UserModel::where('username', 'customer1')->update($data);
+
+    $user = UserModel::all();
+    return view('user', ['data' => $user]);
+
+
+  }
 }
--- POS/routes/web.php
+++ POS/routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\SalesController;
 use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', [UserController::class, 'index']);
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 
@@ -20,6 +21,5 @@ Route::prefix('category')->group(function() {
     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
 });
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 Route::get('/sales', [SalesController::class, 'index']);
 
