c871423201e68769ac6b93932b020f1b10bf31e5
ezreals-ares <nawafazril303@gmail.com>
2026-03-02 11:53:18 +0700

    push praktikum 2.4 JS 4

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -11,8 +11,17 @@ class UserController extends Controller
 {
   public function index() {
 
-    $jumlahUser = UserModel::where('level_id', 2)->count();
-    return view('user', ['jumlahUser' => $jumlahUser]);
+    $user = UserModel::firstOrNew(
+      [
+          'username' => 'manager33',
+          'nama' => 'Manager Tiga Tiga',
+          'password' => Hash::make('12345'),
+          'level_id' => 2
+      ]
+    );
+    $user->save();
+
+    return view('user', ['data' => $user]);
 
   }
 }
