8eb073edd9e4e10add52b5b953e38f8a3ad6a52d
ezreals-ares <nawafazril303@gmail.com>
2026-03-03 09:21:54 +0700

    push praktikum 2 JS 5

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', [UserController::class, 'index']);
@@ -27,3 +28,4 @@ Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+Route::get('/', [WelcomeController::class, 'index']);
